ad594c911819103a0b8643ae583baaff5c81dbae
Yassine Bayoudh <firefistxace1997@gmail.com>
2020-02-08 15:00:57 +0100

    Resolve Fixing Livraison Placement

--- src/Entities/Livraison.java
+++ src/Entities/Livraison.java
@@ -3,9 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Services;
+package Entities;
 
-import Entities.Colis;
 import java.util.Date;
 
 /**
