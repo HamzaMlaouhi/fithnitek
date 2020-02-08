74ebb22555179e05b6c5666cf2e20f3a282f3d8a
dihekmissaoui <60813464+dihekmissaoui@users.noreply.github.com>
2020-02-08 15:49:11 +0100

    resolve fixing reclamation

---
+++ src/Services/ReclamationServise.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services;
+
+import IServices.IReclamationService;
+
+
+
+
+/**
+ *
+ * @author HP
+ */
+public class ReclamationServise implements IReclamationService {
+
+    @Override
+    public void AjouterReclamation() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void SupprimerReclamation() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void RechercherReclamation() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
