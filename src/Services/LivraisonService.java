cdfa5450e644ea105e187211984a9fd635ad545d
ItzWassim <60812823+ItzWassim@users.noreply.github.com>
2020-02-08 15:20:37 +0100

    resolve fixing livraison

---
+++ src/Services/LivraisonService.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services;
+
+import Entities.Livraison;
+import IServices.ILivraisonService;
+import java.util.List;
+
+/**
+ *
+ * @author Wassim
+ */
+public class LivraisonService implements ILivraisonService{
+
+    @Override
+    public void AjouterColis(Livraison l) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void ModifierColis(Livraison l) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void SupprimerColis(Livraison l) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Livraison> AfficherLivraison() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
